2696ec72c9ae3bf2b5e6e6bb00599a4eece4fb6b
Micael Gallego <micael.gallego@gmail.com>
2018-04-13 18:07:20 +0200

    Update java-webapp-bbdd

--- java-webapp-bbdd-enunciado/src/main/java/es/codeurjc/test/tablonanuncios/WebApp.java
+++ java-webapp-bbdd-enunciado/src/main/java/es/codeurjc/test/tablonanuncios/WebApp.java
@@ -2,11 +2,30 @@ package es.codeurjc.test.tablonanuncios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class WebApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebApp.class, args);
+	private static ConfigurableApplicationContext app;
+	
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(WebApp.class, args);
+    }
+    
+    public static void start() {
+    	start(new String[] {});
+    }
+
+	private static void start(String[] args) {
+		if(app == null) {
+    		app = SpringApplication.run(WebApp.class, args);
+    	} 
+	}    
+	
+	public static void stop() {
+		if(app != null) {
+			app.close();
+		}
 	}
 }
